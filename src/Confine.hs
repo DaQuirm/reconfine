16e2c423c71c5187e5aa2b1b28397b5647b4127f
Matthias Fischmann <mf@zerobuzz.net>
2018-01-27 15:13:56 +0100

    ...

---
+++ src/Confine.hs
@@ -0,0 +1,77 @@
+{-# LANGUAGE BangPatterns               #-}
+{-# LANGUAGE ConstraintKinds            #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DefaultSignatures          #-}
+{-# LANGUAGE DeriveFunctor              #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE ExplicitForAll             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE InstanceSigs               #-}
+{-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE PackageImports             #-}
+{-# LANGUAGE PatternSynonyms            #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE StandaloneDeriving         #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TupleSections              #-}
+{-# LANGUAGE TypeApplications           #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeFamilyDependencies     #-}
+{-# LANGUAGE TypeOperators              #-}
+{-# LANGUAGE UndecidableInstances       #-}
+{-# LANGUAGE ViewPatterns               #-}
+
+module Confine where
+
+import Control.Monad.State
+import Control.Monad.Error
+
+type X = Int
+type Y = Int
+type Point = (X, Y)
+
+type Player = String
+
+type Score = Int
+
+-- | in 'Done', all 'Player's are listed.  list inside list contains 'Player's with same score.
+data GameState = InProgress | Done [[(Player, Score)]]
+  deriving (Eq, Ord, Show)
+
+emptyState :: GameState
+emptyState = InProgress
+
+data GameError = InternalOutOfBounds Point | EdgeAlreadySet Point Edge
+  deriving (Eq, Ord, Show)
+
+type Eff m {- :: Constraint -} = (MonadState GameState m, MonadError GameError m)
+
+
+getOccupant :: Eff m => Point -> m Player
+getOccupant = _
+
+setOccupant :: Eff m => Player -> Point -> m ()
+setOccupant = _
+
+
+data Edge = TopEdge | BottomEdge | LeftEdge | RightEdge
+  deriving (Eq, Ord, Show)
+
+getEdge :: Eff m => Point -> Edge -> m Bool
+getEdge = _
+
+-- | you can only switch once from False to True.
+setEdge :: Eff m => Point -> Edge -> m ()
+setEdge = _
+
+
+-- | 'setEdge', then potentially 'setOccupant' of (one of) the two affected cells.
+move :: Eff m => Player -> Point -> Edge -> m GameState
+move = _
---
+++ test/ConfineSpec.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE BangPatterns               #-}
+{-# LANGUAGE ConstraintKinds            #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE DefaultSignatures          #-}
+{-# LANGUAGE DeriveFunctor              #-}
+{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE ExplicitForAll             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE InstanceSigs               #-}
+{-# LANGUAGE LambdaCase                 #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE PackageImports             #-}
+{-# LANGUAGE PatternSynonyms            #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE StandaloneDeriving         #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TupleSections              #-}
+{-# LANGUAGE TypeApplications           #-}
+{-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE TypeFamilyDependencies     #-}
+{-# LANGUAGE TypeOperators              #-}
+{-# LANGUAGE UndecidableInstances       #-}
+{-# LANGUAGE ViewPatterns               #-}
+
+module ConfineSpec where
+
+import Test.Hspec
+
+import Confine
+
+
+spec :: Spec
+spec = do
+  describe "getOccupant" $ do
+    it "works" pending
+
+  describe "setOccupant" $ do
+    it "works" pending
+
+  describe "getEge" $ do
+    it "works" pending
+
+  describe "setEge" $ do
+    it "works" pending
+
+  describe "move" $ do
+    it "works" pending
